f10c2cc697863751f2291d80ed2ae267b5339819
Clara <clara.twq@gmail.com>
2026-08-27 08:29:57 +0800

    configure to export the app as a web application and listen on port PORT provided by Render.

--- main.py
+++ main.py
@@ -200,4 +200,5 @@ def main(page: ft.Page):
 
 
 if __name__ == "__main__":
-    ft.app(main)
+    port = int(os.environ.get("PORT", 8080))
+    ft.app(target=main, view=ft.AppView.WEB_BROWSER, host="0.0.0.0", port=port)
